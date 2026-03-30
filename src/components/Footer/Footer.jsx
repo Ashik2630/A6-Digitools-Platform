dafd1b9627a9c8750734a0f9104101fe7b1ca42c
Ashik2630 <ashik426370@gmail.com>
2026-03-31 04:51:21 +0600

    Update My footer section

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,38 +3,45 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="bg-[#101727] text-white py-20 ">
-      <div className="container mx-auto flex-wrap md:flex justify-evenly">
+      <div className="container mx-auto flex-wrap text-center md:flex justify-evenly">
         <div>
-          <h1>DigiTools</h1>
-          <p>
+          <h1 className="text-4xl font-bold mb-5">DigiTools</h1>
+          <p className="text-[#ffffffa9]">
             Premium digital tools for creators, <br /> professionals, and
             businesses. Work smarter <br /> with our suite of powerful tools.
           </p>
         </div>
         <div>
-          <h4>Product</h4>
-          <p>Features</p>
-          <p>Pricing</p>
-          <p>Templates</p>
-          <p>Integrations</p>
+          <h4 className="text-xl font-medium mb-5">Product</h4>
+          <div className="space-y-5 text-[#ffffffa9]">
+            <p>Features</p>
+            <p>Pricing</p>
+            <p>Templates</p>
+            <p>Integrations</p>
+          </div>
         </div>
         <div>
-          <h4>Company</h4>
-          <p>About</p>
-          <p>Blog</p>
-          <p>Careers</p>
-          <p>Press</p>
+          <h4 className="text-xl font-medium mb-5">Company</h4>
+          <div className="space-y-5 text-[#ffffffa9]">
+            <p>About</p>
+            <p>Blog</p>
+            <p>Careers</p>
+            <p>Press</p>
+          </div>
         </div>
         <div>
-          <h4>Resources</h4>
-          <p>Documentation</p>
-          <p>Help Center</p>
-          <p>Community</p>
-          <p>Contact</p>
+          <h4 className="text-xl font-medium mb-5">Resources</h4>
+          <div className="space-y-5 text-[#ffffffa9]">
+            <p>Documentation</p>
+            <p>Help Center</p>
+            <p>Community</p>
+            <p>Contact</p>
+          </div>
         </div>
         <div>
-          <h4>Social Links</h4>
-          <a href="/">
+          <h4 className="text-xl font-medium mb-1">Social Links</h4>
+          <div className="space-x-5">
+            <a href="/">
             <i class="fa-brands fa-instagram"></i>
           </a>
           <a href="/">
@@ -43,11 +50,12 @@ const Footer = () => {
           <a href="/">
             <i class="fa-brands fa-x-twitter"></i>
           </a>
+          </div>
         </div>
-        <hr className="bg-red-300 w-[85%] mx-auto" />
-        <div className="flex gap-180">
-          <p>© 2026 Digi-tools. All rights reserved.</p>
-          <div>
+        <hr className="bg-red-300 w-[85%] mx-auto mt-10 mb-4 text-[#4b4c4f]" />
+        <div className="flex-wrap md:flex gap-5 md:gap-180 ">
+          <p className="text-[#ffffffa9] mb-2 md:mb-0">© 2026 Digi-tools. All rights reserved.</p>
+          <div className="flex gap-5 text-[#ffffffa9] ml-20 md:ml-0">
             <a href="/">Privacy Policy</a>
             <a href="/">Terms of Service</a>
             <a href="/">Cookies</a>
